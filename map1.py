425b7220ae8f725a59df92d7c0019a43429d58dd
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-15 00:51:32 +0530

    adding markers of volcanoes

--- map1.py
+++ map1.py
@@ -17,5 +17,10 @@ fg = folium.FeatureGroup(name="my map")
 
 for lt, ln, el in zip(lat,lon,elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 6, popup=str(el)+" m", fill_color = color_elev(el),color = "grey", fill_opacity = 0.7))
+
+
+
 map.add_child(fg)
+
+map.add_child(folium.LayerControl())
 map.save("map1.html")
